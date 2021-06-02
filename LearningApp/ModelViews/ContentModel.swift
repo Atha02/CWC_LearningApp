5de65052f8a1d79a87c45141cb769668d1e0715a
Athanasios Chatzikonstantinou <atha2015@icloud.com>
2021-06-02 13:10:03 +0200

    - files into folders - rename content view - set environment object - create viewmodel

---
+++ LearningApp/ModelViews/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Athanasios Chatzikonstantinou on 02.06.21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  LearningApp
 //
 //  Created by Athanasios Chatzikonstantinou on 02.06.21.
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
